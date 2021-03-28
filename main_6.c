8a689d4864501bb7e7e2a089c1a00182b3ebd50d
Herbert Andrade Nascimento <hanascimento.cic@uesc.br>
2021-03-28 23:47:31 +0000

    add Mega senna Ex.6 ainda com problemas

--- main_6.c
+++ main_6.c
@@ -2,42 +2,92 @@
 #include <stdlib.h>  	//para poder utilizar a função rand()
 #include <time.h> 		//para poder utilizar a funçao time()	
 
-void jogaOsDados(int min, int tam, int qtVezes, int semente);
+#define QUANT_JOGOS 3
+
+int *GerarVetorNaoRepetido(int, int, int, int);
+void retirarRepeticoes(int *, int);
+void bubble_sort (int vetor[], int n);
+void printVet(int *,int);
+int gerarSemente();
+void freeVet(int *);
+void ordenar(int *,int);
 
 int main(void) { // Ponto de entrada do Programa 
 
-	printf("Implementando funções\n");
-	printf("A função rand() gera um inteiro entre 0 e RAND_MAX\n");
-	printf("RAND_MAX = %d\n", RAND_MAX);
-	printf("Simulando 10 lançamentos de um dado:\n");
-	jogaOsDados(1, 6, 10, 0);
-	printf("Todas as vezes que rodar este exemplo vai gerar o mesmo resultado!\n");
-	int semente;
-	printf("Entre com a semente para gerar uma nova sequencia: ");
-	int err = scanf("%d", &semente);
-	jogaOsDados(1, 6, 10, semente);
-	printf("Sementes diferentes irão gerar resultados diferentes!\n");
-	printf("A mesma semente gera o mesmo resultado!\n");
-
-	printf("Escolhendo a semente de forma automatica\n");
-	jogaOsDados(1, 6, 10, -1);
-	printf("Todas as vezes que rodar este exemplo gera diferentes resultados!\n");
+	printf("MEGA SENNA\n");
+	printf("Sorteando numeros:\n");
+  int *jogo, semente;
+  for(int i=1;i<=QUANT_JOGOS;i++){
+      printf("Digite a semente: ");
+      scanf("%d", &semente);
+      jogo = GerarVetorNaoRepetido(1, 60, 6, semente);
+      ordenar(jogo,6);
+      printVet(jogo,6);
+  }  
+  freeVet(jogo); 
 	return 0;
 }
 
-void jogaOsDados(int min, int tam, int qtVezes, int semente){
+int *GerarVetorNaoRepetido(int min, int tam, int qtVezes,int semente){
+  int *vetor;
+  vetor = malloc(tam*sizeof(int));
+  int i, j, rep;
+  srand(semente);
+  for(i=0;i<qtVezes;i++){
+    do{
+      vetor[i] = min+rand()%tam;
+      rep = 0;
+      for(j=0;j<i;j++){
+        if(vetor[i]==vetor[j]){
+            rep=1;
+            break;
+        }
+      }
+    }while(rep);    
+  }
+  return vetor;
+}
+
+ /* int *vet;
+  vet = malloc(tam*sizeof(int));
 	if (semente > 0)
 		srand(semente);
 	else if(semente < 0)
 		srand(time(NULL));
-	printf("( ");
 	int dado;
 	for(int i = 1; i <= qtVezes; i++){
 		dado = min + rand()%tam;
-		printf("%d", dado);
-		if (i < qtVezes)
+    vet[i]=dado;
+	}
+  retirarRepeticoes(vet,tam);
+  if(strlen(vet)==tam);
+  return vet;
+}*/
+
+void printVet(int *vet,int tam){
+  printf("( ");
+  for(int i = 0; i < tam; i++){
+		printf("%d", vet[i]);
+		if (i < tam-1)
 			printf(", ");
 		else
 			printf(")\n");
 	}
+}
+
+void freeVet(int *vet){
+  free(vet);
+}
+
+void ordenar(int *vetor,int tam){
+  int aux;
+  for (int i = 0; i < tam; i++){
+      for (int j = 0; j < tam; j++){
+        if (vetor[i] < vetor[j]){
+            aux = vetor[i];
+            vetor[i] = vetor[j];
+            vetor[j] = aux;
+        }
+    }
+  }
 }
