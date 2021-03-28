083f5eba6f5c4c846a8a5f2f4f0b036c990a2600
Herbert Andrade Nascimento <hanascimento.cic@uesc.br>
2021-03-28 04:01:50 +0000

    nenhuma

--- main.c
+++ main.c
@@ -6,8 +6,6 @@
 int main(int argc, char *argv[]) { // Ponto de entrada do Programa 
   
 	char name[100]; //name[0], name
-  printf("argc: %d", argc);
-  printf("argv: %s", argv[0]);
 	printf("Hello World!\n");    //output
 	if(argc > 1)
 		printf("Hi %s, good to see you here!!!\n", argv[1]);
