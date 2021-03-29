b513c751239044cb6693d7ba6f0a36401579add8
Herbert Andrade Nascimento <hanascimento.cic@uesc.br>
2021-03-29 00:16:32 +0000

    add 7b

--- main_6.c
+++ main_6.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>  	//para poder utilizar a função rand()
 #include <time.h> 		//para poder utilizar a funçao time()	
+#include <string.h>
 
 #define QUANT_JOGOS 3
 
@@ -9,20 +10,40 @@ void retirarRepeticoes(int *, int);
 void printVet(int *,int);
 void freeVet(int *);
 void ordenar(int *,int);
+int compararJogo(int *, int *, int );
 
 int main(void) { // Ponto de entrada do Programa 
 
 	printf("MEGA SENNA\n");
 	printf("Sorteando numeros:\n");
-  int *jogo, semente;
-  for(int i=1;i<=QUANT_JOGOS;i++){
-      printf("Digite a semente: ");
-      scanf("%d", &semente);
-      jogo = GerarVetorNaoRepetido(1, 60, 6, semente);
-      ordenar(jogo,6);
-      printVet(jogo,6);
-  }  
-  freeVet(jogo); 
+  int *jogo_1,*jogo_2,*jogo_3, semente;
+
+  printf("Digite a nova semente: ");
+  scanf("%d", &semente);
+  jogo_1 = GerarVetorNaoRepetido(1, 60, 6, semente);
+  ordenar(jogo_1,6);
+  printVet(jogo_1,6);
+
+  printf("Digite a nova semente: ");
+  scanf("%d", &semente);
+  jogo_2 = GerarVetorNaoRepetido(1, 60, 6, semente);
+  ordenar(jogo_2,6);
+  printVet(jogo_2,6);
+
+  printf("Digite a nova semente: ");
+  scanf("%d", &semente);
+  jogo_3 = GerarVetorNaoRepetido(1, 60, 6, semente);
+  ordenar(jogo_3,6);
+  printVet(jogo_3,6);
+
+  int rep;
+  rep = compararJogo(jogo_1,jogo_2,6);
+  printf("%d números se repetem em ambos os jogos\n", rep);
+
+
+  freeVet(jogo_1);
+  freeVet(jogo_2); 
+  freeVet(jogo_3);  
 	return 0;
 }
 
@@ -72,4 +93,19 @@ void ordenar(int *vetor,int tam){
         }
     }
   }
+}
+
+int compararJogo(int *vet_1, int *vet_2, int tam){
+  int sentinela, cont=0;
+  for(int i=0;i<tam;i++){
+    sentinela=0;
+    for(int j=0;j<i;j++)
+      if(vet_1[i] == vet_2[j]){
+        sentinela = 1;
+      }
+    if(sentinela==1)
+      cont++;
+  }
+  return cont;
+
 }
